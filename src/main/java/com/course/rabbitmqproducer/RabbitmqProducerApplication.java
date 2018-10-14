d39c3d9160fbbc8b3a64b1319b85dc0e27a3fd2f
Unknown <guileme13@gmail.com>
2018-10-14 14:07:39 -0300

    Fanout exchange

--- src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
+++ src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -6,20 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.course.rabbitmqproducer.producer.EmployeeJsonProducer;
+import com.course.rabbitmqproducer.producer.HumanResourceProducer;
 import com.course.rabbitmqproducer.producer.entity.Employee;
 
 @SpringBootApplication
-//@EnableScheduling
 public class RabbitmqProducerApplication implements CommandLineRunner {
 	
 	@Autowired
-	private EmployeeJsonProducer employeeJsonProducer;
+	private HumanResourceProducer humanResourceProducer;
 	
-//	@Autowired
-//	private HelloRabbitProducer helloRabbitProducer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqProducerApplication.class, args);
@@ -27,11 +23,10 @@ public class RabbitmqProducerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-//		helloRabbitProducer.sendHello("Fioritti " + Math.random());
 		
 		for(int i = 0;i< 5;i++) {
 			Employee employee = new Employee("emp" + i, "Employee " + i, new Date());
-			employeeJsonProducer.sendMessage(employee);
+			humanResourceProducer.sendMessage(employee);
 			System.out.println("Employee sent");
 		}
 		
