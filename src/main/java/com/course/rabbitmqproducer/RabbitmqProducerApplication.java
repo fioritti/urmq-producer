59c0359d33fea2e73643795db6d010d30335bdb4
Unknown <guileme13@gmail.com>
2018-10-14 14:50:22 -0300

    Direct exchange

--- src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
+++ src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -1,21 +1,24 @@
 package com.course.rabbitmqproducer;
 
-import java.util.Date;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.course.rabbitmqproducer.producer.HumanResourceProducer;
-import com.course.rabbitmqproducer.producer.entity.Employee;
+import com.course.rabbitmqproducer.producer.PictureProducer;
+import com.course.rabbitmqproducer.producer.entity.Picture;
 
 @SpringBootApplication
 public class RabbitmqProducerApplication implements CommandLineRunner {
-	
+
 	@Autowired
-	private HumanResourceProducer humanResourceProducer;
-	
+	private PictureProducer pictureProducer;
+	private final List<String> TYPES = Arrays.asList("jpg", "png", "svg");
+	private final List<String> SOURCES = Arrays.asList("mobile", "web");
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqProducerApplication.class, args);
@@ -23,12 +26,16 @@ public class RabbitmqProducerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		for(int i = 0;i< 5;i++) {
-			Employee employee = new Employee("emp" + i, "Employee " + i, new Date());
-			humanResourceProducer.sendMessage(employee);
-			System.out.println("Employee sent");
+
+		for (int i = 0; i < 10; i++) {
+			Picture p = new Picture();
+			p.setName("Picture" + i);
+			p.setSize(ThreadLocalRandom.current().nextLong(1, 10000));
+			p.setSource(SOURCES.get(i % SOURCES.size()));
+			p.setType(TYPES.get(i % TYPES.size()));
+			
+			pictureProducer.sendMessage(p);
 		}
-		
+
 	}
 }
